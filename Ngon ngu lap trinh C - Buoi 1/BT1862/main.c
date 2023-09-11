e0037d6a0da1d254607549fc684aac1c914a6f96
Moon-8890 <nguyet.dn.2354@aptechlearning.edu.vn>
2023-09-11 21:24:11 +0700

    C2307L

---
+++ Ngon ngu lap trinh C - Buoi 1/BT1862/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+
+printf ("\n*"); 
+printf ("\n**");
+printf ("\n***"); 
+printf ("\n****"); 
+printf ("\n*****"); 
+
+printf ("\n========================="); 
+printf ("\n|STT|Ho Ten  |Tuoi |Gioi Tinh|"); 
+printf ("\n|01 |NguyenA |20   |Nam      |");
+printf ("\n|02 |NguyenB |22   |Nam      |");
+printf ("\n|03 |NguyenC |28   |Nam      |");
+
+int a=1, b=2, c=3, d=4, e=5, f=6;
+d= 10*a*a + 5*b*c + c*c;
+printf ("\nd= %d", d);
+
+	return 0;
+}
---
+++ Ngon ngu lap trinh C - Buoi 1/BT2524/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("\n Dang Thi Nhu Nguyet");
+	printf("\n Tuoi: 33");
+	printf("\n Dia chi: Ha Noi");
+	printf("\n Email: nguyet.dn.2354@aptechlearning.edu.vn");
+	printf("\n SDT: 0974781436");
+	return 0;
+}
